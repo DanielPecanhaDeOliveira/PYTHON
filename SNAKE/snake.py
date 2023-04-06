572600e92c453fc2ffa3e8b1f70773840ae596d3
DANIEL PECANHA DE OLIVEIRA <129777679+DanielPecanhaDeOliveira@users.noreply.github.com>
2023-04-06 09:45:23 -0300

    Criando a tela do game

--- SNAKE/snake.py
+++ SNAKE/snake.py
@@ -1,1 +1,19 @@
-#Criando snake em python
+import pygame
+#Inicializando módulos de Pygame
+pygame.init()
+
+#Criando uma janela com o titulo "Snake"
+janela = pygame.display.set_caption('Snake')
+#Definindo tamanho da tela 
+pygame.display.set_mode((400, 300))
+
+loop = True
+#Loop do jogo
+while loop:
+    #Checando eventos
+    for event in pygame.event.get():
+        #Se for um evento QUIT('Saida')
+        if event.type == pygame.QUIT:
+            loop = False
+#Encerrando módulos de Pygame
+pygame.quit()
