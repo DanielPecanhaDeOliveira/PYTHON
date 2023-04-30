c697667d2b8106c39fd43a6f493bd1528271a199
DANIEL PECANHA DE OLIVEIRA <129777679+DanielPecanhaDeOliveira@users.noreply.github.com>
2023-04-29 22:23:46 -0300

    Atualizando codigo

--- JOKEMPO/jokempo.py
+++ JOKEMPO/jokempo.py
@@ -2,46 +2,61 @@
 import random
 from time import sleep
 #Criando cabeçalho
-print('=' *20)
-print('{:^30}'.format('\033[32mJOKENPÔ\033[m'))
-print('=' *20)
-opçoes = ['PEDRA', 'PAPEL','TESOURA']
-pc = random.choice(opçoes)
-#Criando menu    
-menu = ('''\n[0] PEDRA 
+def cabeçalho(msg):
+    linha('\033[31m', '\033[m')
+    print(msg.center(32))
+    linha('\033[31m', '\033[m')
+
+
+def linha (abrecor, fechacor):
+    print(f'{abrecor}={fechacor}' *25)
+
+
+c = 1
+while True:
+    cabeçalho("\033[34mJOKENPÔ\033[m")
+    opçoes = ['PEDRA', 'PAPEL','TESOURA']
+    pc = random.choice(opçoes)
+    #Criando menu
+    menu = ('''\n[0] PEDRA 
 [1] PAPEl 
 [2] TESOURA\n''')
-print(menu)
-print('=' *20)
-user = int(input('Qual vc escolhe? '))
-sleep(1.5)
-print('='*25)
-#Mostrando as opções escolhidas
-print(f'O computador jogou {pc}')
-print(f'Você jogou {opçoes[user]} ')
-print('='*25, '\n', end='\033[32m')
-sleep(2)
-#Verificando o ganhador 
-if user == 0:
-    if pc == 'PEDRA':
-        print('\033[33mEMPATE!\033[m')
-    if pc == 'PAPEL':
-        print('\033[31mO computador ganhou!\033[m')
-    if pc == 'TESOURA':
-        print('Você ganhou!')
-if user == 1:
-    if pc == 'PEDRA':
-        print('\033[32mVocê ganhou!\033[m')
-    if pc == 'PAPEL':
-        print('\033[33mEMPATE!\033[m')
-    if pc == 'TESOURA':
-        print('\033[31mO computador ganhou!\033[m')
-if user == 2:
-    if pc == 'PEDRA':
-        print('\033[31mO computador ganhou!\033[m')
-    if pc == 'PAPEL':
-        print('Você ganhou!')
-    if pc == 'TESOURA':
-        print('\033[33mEMPATE!\033[m')
-print('\033[m='*25)
+    print(menu)
+
+    user = int(input('Qual vc escolhe? '))
+    sleep(1)
+    linha('\033[31m', '\033[m')
+    #Mostrando as opções escolhidas
+    print(f'O computador jogou {pc}')
+    print(f'Você jogou {opçoes[user]} ')
+    linha('\033[31m', '\033[m')
+    sleep(2)
+    c  += 1
+    #Verificando o ganhador
+    if user == 0:
+        if pc == 'PEDRA':
+            print('\033[33mEMPATE!\033[m'.center(30))
+        if pc == 'PAPEL':
+            print('\033[31mO computador ganhou!\033[m'.center(30))
+        if pc == 'TESOURA':
+            print('\033[32mVocê ganhou!\033[m'.center(30))
+    if user == 1:
+        if pc == 'PEDRA':
+            print('\033[32mVocê ganhou!\033[m'.center(30))
+        if pc == 'PAPEL':
+            print('\033[33mEMPATE!\033[m'.center(30))
+        if pc == 'TESOURA':
+            print('\033[31mO computador ganhou!\033[m'.center(30))
+    if user == 2:
+        if pc == 'PEDRA':
+            print('\033[31mO computador ganhou!\033[m'.center(30))
+        if pc == 'PAPEL':
+            print('\033[32mVocê ganhou!\033[m'.center(30))
+        if pc == 'TESOURA':
+            print('\033[33mEMPATE!\033[m'.center(30))
+    print('\033[m='*25)
+    sleep(1)
+    cabeçalho(f"\033[34mRODADA\033[m \033[31m{c}\033[mº".center(42))
+    sleep(1)
+
 
